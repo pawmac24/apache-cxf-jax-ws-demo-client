d24801eac18222538682e8c6f540c0ec559bf47e
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-09-09 16:52:46 +0200

    Third client stage - implementing web service call

--- src/main/java/com/pluralsight/cxfdemo/orders/OrdersController.java
+++ src/main/java/com/pluralsight/cxfdemo/orders/OrdersController.java
@@ -1,31 +1,39 @@
 package com.pluralsight.cxfdemo.orders;
 
+import com.pluralsight.schema.order.ObjectFactory;
+import com.pluralsight.schema.order.OrderInquiryResponseType;
+import com.pluralsight.schema.order.OrderInquiryType;
+import com.pluralsight.service.orders.Orders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Controller for processing orders through the Orders web service.  
- * 
- * @author Michael Hoffman, Pluralsight
+ * Controller for processing orders through the Orders web service.
  *
+ * @author Michael Hoffman, Pluralsight
  */
 @Controller("/processOrderPlacement")
 public class OrdersController {
 
-   // TODO: Add the Orders interface as a member of the controller
-   
-   @RequestMapping(method = RequestMethod.GET)
-   public String processOrderPlacement(ModelMap model) throws Exception {
-      // TODO: Add the creation of an OrderInquiry request type and populate it
-      
-      // TODO: Call the orders web service      
-      
-      // TODO: Replace null with the order status from the web service response.
-      model.addAttribute("orderStatus", null);
-      
-      // View we are returning to, in this case processOrderPlacement.jsp 
-      return "processOrderPlacement";
-   }
+    @Autowired
+    private Orders orders;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String processOrderPlacement(ModelMap model) throws Exception {
+        OrderInquiryType orderInquiry = new ObjectFactory().createOrderInquiryType();
+        orderInquiry.setAccountId(1234);
+        orderInquiry.setEan13(1234567890123L);
+        orderInquiry.setOrderQuantity(2);
+        orderInquiry.setUniqueOrderId(999);
+
+        OrderInquiryResponseType response = orders.processOrderPlacement(orderInquiry);
+
+        model.addAttribute("orderStatus", response.getOrder().getOrderStatus().value());
+
+        // View we are returning to, in this case processOrderPlacement.jsp
+        return "processOrderPlacement";
+    }
 }
